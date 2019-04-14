39619f2102f4c7806b22321ac67578ed44698a98
Pastor-Loic <44533866+LoPastor@users.noreply.github.com>
2019-04-14 11:59:00 +0200

    Add files via upload
    modification et suppression d'un jeu rajouté en local
    nous pouvons modifier la description, le nom et la console d'un jeu choisi
    un refresh remet les données comme à l'origine
    2e étape : suppression en dur (persiste même après refresh)

---
+++ components/AjoutRom.vue
@@ -0,0 +1,67 @@
+<template>
+    <div id="ajoutRom">
+
+        <label class="file-select">
+            <div class="select-button">
+                <span v-if="false">Selected File: </span>
+                <span v-else>AJOUTER ROM</span>
+            </div>
+            <input type="file"/>
+        </label>
+
+
+        <v-card-text>
+            <p class="text-md-center">A définir plus tard...{{msg}}</p>
+        </v-card-text>
+
+    </div>
+
+</template>
+
+
+<script>
+    export default {
+        name: "AjoutRom"
+    }
+
+</script>
+
+<style>
+
+    div#ajoutRom{
+        margin-top: 100px;
+        align-items: center;
+        align-content: center;
+
+    }
+
+    .file-select > .select-button:hover{
+        background-color: #dce4dd;
+    }
+
+    .file-select > .select-button {
+
+        margin-left: 45%;
+        vertical-align: middle;
+
+        box-shadow: 0 2px 2px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
+
+        padding: 1rem;
+
+        cursor: pointer;
+
+        color: black;
+        background-color: #eaf1ea;
+
+      /*  border-radius: .3rem; */
+
+        text-align: center;
+        font-weight: bold;
+        width: 130px;
+        height: 35px;
+    }
+
+    .file-select > input[type="file"] {
+        display: none;
+    }
+</style>
